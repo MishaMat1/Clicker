d42af77d5d1e58604ef120f7349082e55beec4e4
MishaMat1 <mistermat2016@gmail.com>
2025-04-13 22:13:22 +0300

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -1,11 +1,15 @@
 var points = 0;
-var number = 1;
+var number = 0;
 var number2 = 1;
+var number3 = 1;
+var number4 = 1;
 var ClickPower = 0;
 var ClickMultiplier = 0;
+var ClickCompound = 0;
 var upgradeCost = 25;
+
 function pointClick(){
-    number = (1 + ClickMultiplier) * (1 + ClickPower);
+    number = number2 * number3 * number4;
     points = points + number;
     document.getElementById("points").innerHTML = points;
 };
@@ -16,8 +20,9 @@ function updateTexts() {
 
 function upgradeClick() {
     if (points >= upgradeCost) {
-    ClickPower = ClickPower + 1;
     points = points - upgradeCost;
+    ClickPower = ClickPower + 1;
+    number2 = ClickPower + 1;
     document.getElementById("upgrade").innerHTML = ClickPower;
     updateTexts();
     }
@@ -25,10 +30,20 @@ function upgradeClick() {
 
 function upgradeClick2() {
     if (points >= upgradeCost*2) {
-    ClickMultiplier = ClickMultiplier + 1;
     points = points - upgradeCost*2;
-    number2 = ClickMultiplier*1 + 1;
+    ClickMultiplier = ClickMultiplier + 1;
+    number3 = ClickMultiplier*1 + 1;
     document.getElementById("upgrade2").innerHTML = ClickMultiplier;
+    updateTexts();
+    }
+}
+
+function upgradeClick3() {
+    if (points >= upgradeCost*40) {
+    points = points - upgradeCost*40;
+    ClickCompound = ClickCompound + 1;
+    number4 = 2**ClickCompound;
+    document.getElementById("upgrade3").innerHTML = ClickCompound;
     updateTexts();
     }
 }
